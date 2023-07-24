c08ab131e4a64b1bc122bc9a298c39775aac48db
perp1exbro <137335593+perp1exbro@users.noreply.github.com>
2023-07-24 03:50:09 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa(int m, int n)
+{
+if (m==n)
+return n;
+else return m + summa(m + 1, n);
+}
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m,n)}");
